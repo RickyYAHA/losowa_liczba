b7735a3537aaf4a485b643efb0789716a8409d36
RickyYAHA <100943801+RickyYAHA@users.noreply.github.com>
2022-05-19 19:14:32 +0200

    Add files via upload

---
+++ losowa/losowa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int losowa = rnd.Next(1, 10);
+        while (true)
+        {
+            Console.Write("Podaj liczbe: ");
+            int co = Int32.Parse(Console.ReadLine());
+            if (co == losowa)
+            {
+                Console.WriteLine("Gratuluje, trafiles prawidlowa liczbe!");
+                Environment.Exit(0);
+            }
+            else if (co > losowa)
+            {
+                Console.WriteLine("Podana liczba jest zbyt wysoka");
+            }
+            else if (co < losowa)
+            {
+                Console.WriteLine("Podana liczba jest zbyt niska");
+            }
+        }
+    }
+}
